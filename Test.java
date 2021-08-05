1f7379057d1ec8cbf7418b72bd0ef51c73fa2c38
Oliver Robles <82982066+oliverrobles@users.noreply.github.com>
2021-08-05 02:30:32 -0500

    Ejercicio 3

--- Test.java
+++ Test.java
@@ -6,28 +6,30 @@ public class Test {
         Grafo Grafo1 = new Grafo();
         
         int tam = 5;
-	      Vertice [] vertices = new Vertice[tam];
-	      char [] etiquetas = {'Z','X','V','T','R'};
+	System.out.println("Lista de adyacencia\n");
+	Vertice [] vertices = new Vertice[tam];
+	char [] etiquetas = {'Z','X','V','T','R'};
 
-	      for(int i = 0; i < vertices.length; i++){
-	    	    vertices[i] = new Vertice(Character.toString(etiquetas[i]));
-	     	    System.out.println(vertices[i]);	    	
-	      }
+	for(int i = 0; i < vertices.length; i++){
+		vertices[i] = new Vertice(Character.toString(etiquetas[i]));
+	     	System.out.println(vertices[i]);	    	
+	}
 
-	      System.out.println();
+	System.out.println();
 
-	      Grafo1.insertarArista(vertices[4], vertices[2], 10);
-	      Grafo1.insertarArista(vertices[3], vertices[0], 6);
-	      Grafo1.insertarArista(vertices[4], vertices[3], 40);	
-	      Grafo1.insertarArista(vertices[4], vertices[3], 38);
-	      Grafo1.insertarArista(vertices[3], vertices[2], 39);
-	      Grafo1.insertarArista(vertices[1], vertices[4], 25);
+	Grafo1.insertarArista(vertices[4], vertices[2], 10);
+	Grafo1.insertarArista(vertices[3], vertices[0], 6);
+	Grafo1.insertarArista(vertices[4], vertices[3], 40);	
+	Grafo1.insertarArista(vertices[4], vertices[3], 38);
+	Grafo1.insertarArista(vertices[3], vertices[2], 39);
+	Grafo1.insertarArista(vertices[1], vertices[4], 25);
         
         Grafo1.listAdyacencia(vertices);
 	
 	
 	GrafoDFS g2 = new GrafoDFS(6);
- 
+ 	
+	System.out.println("DFS\n");
         g2.addDir(0, 1);
         g2.addDir(0, 2);
         g2.addDir(1, 0);
@@ -46,7 +48,8 @@ public class Test {
 	    
 	    
 	GrafoBFS grafo3 = new GrafoBFS(6);
- 
+ 	
+	System.out.println("BFS\n");
         grafo3.addDir(3, 5);
         grafo3.addDir(4, 5);
         grafo3.addDir(1, 3);
@@ -63,6 +66,31 @@ public class Test {
         System.out.println("El recorrido en anchura es la siguiente:");
  
         grafo3.BFS(0);
+	    
+	    
+	Dijkstra g4 = new Dijkstra(GrafoD);
+        g4.dijkstra(START);
+        System.out.println("Dijkstra\n");
+        System.out.println("Camino mas corto:");
+        g4.camino(END);
+        System.out.println("\n\n");
+        
+        System.out.println("Todos los caminos:");
+        g4.caminos();
     }
     
+    private static final Dijkstra.Arista[] GrafoD = {
+
+        new Dijkstra.Arista("a", "b", 7),
+        new Dijkstra.Arista("a", "c", 9),
+        new Dijkstra.Arista("a", "f", 14),
+        new Dijkstra.Arista("b", "c", 10),
+        new Dijkstra.Arista("b", "d", 15),
+        new Dijkstra.Arista("c", "d", 11),
+        new Dijkstra.Arista("c", "f", 2),
+        new Dijkstra.Arista("d", "e", 6),
+        new Dijkstra.Arista("e", "f", 9),
+    };
+    private static final String START = "a";
+    private static final String END = "e";
 }
