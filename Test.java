decb38032c8324c6817a2652288e2aef8e200a06
Oliver Robles <82982066+oliverrobles@users.noreply.github.com>
2021-08-05 01:30:19 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -26,23 +26,43 @@ public class Test {
         Grafo1.listAdyacencia(vertices);
 	
 	
-	GrafoDFS g = new GrafoDFS(6);
+	GrafoDFS g2 = new GrafoDFS(6);
  
-        g.addDir(0, 1);
-        g.addDir(0, 2);
-        g.addDir(1, 0);
-        g.addDir(1, 3);
-        g.addDir(2, 0);
-        g.addDir(2, 3);
-        g.addDir(3, 4);
-        g.addDir(3, 5);
-        g.addDir(4, 3);
-        g.addDir(5, 3);
+        g2.addDir(0, 1);
+        g2.addDir(0, 2);
+        g2.addDir(1, 0);
+        g2.addDir(1, 3);
+        g2.addDir(2, 0);
+        g2.addDir(2, 3);
+        g2.addDir(3, 4);
+        g2.addDir(3, 5);
+        g2.addDir(4, 3);
+        g2.addDir(5, 3);
  
         System.out.println(
             "A continuación se muestra el recorrido en profundidad: ");
  
-        g.DFS(0);
+        g2.DFS(0);
+	    
+	    
+	GrafoBFS grafo3 = new GrafoBFS(6);
+ 
+        grafo3.addDir(3, 5);
+        grafo3.addDir(4, 5);
+        grafo3.addDir(1, 3);
+        grafo3.addDir(1, 4);
+        grafo3.addDir(1, 2);
+        grafo3.addDir(0, 1);
+        grafo3.addDir(1, 0);
+        grafo3.addDir(2, 5);
+        grafo3.addDir(5, 1);
+        grafo3.addDir(5, 2);
+        grafo3.addDir(0, 5);
+        grafo3.addDir(0, 3);
+ 
+        System.out.println("El recorrido en anchura es la siguiente:");
+ 
+        grafo3.BFS(0);
     }
     
 }
